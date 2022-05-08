db26cd8d56d0bcfeb0aa27d06f053b799ca546ed
ericchi00 <ericchi00@gmail.com>
2022-05-08 13:32:16 -0700

    category & manufacturer ref

--- models/computerpart.js
+++ models/computerpart.js
@@ -4,6 +4,8 @@ const { Schema } = mongoose;
 
 const ComputerPartSchema = new Schema({
 	name: { type: String, required: true, maxlength: 100 },
+	category: { type: Schema.Types.ObjectId, ref: 'Category' },
+	manufacturer: { type: Schema.Types.ObjectId, ref: 'Manufacturer' },
 	description: { type: String },
 	picture: { type: mongoose.SchemaTypes.Url },
 	price: { type: Number, required: true },
