cfc8881c7ff56b7ed9927fd599e59113520983a3
ericchi00 <ericchi00@gmail.com>
2022-05-08 13:04:05 -0700

    added in ejs and error page

--- app.js
+++ app.js
@@ -1,31 +1,40 @@
 import express from 'express';
 import path from 'path';
 
-import __dirname  from './dirname.js';
-import cookieParser  from 'cookie-parser';
-import cors  from 'cors';
-import logger  from 'morgan';
+import __dirname from './dirname.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import logger from 'morgan';
 
-import usersRouter  from './routes/users.js';
+import indexRouter from './routes/index.js';
+import usersRouter from './routes/users.js';
 
 const app = express();
+// const mongoDB = process.env.MONGODB_URI;
+// mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public'), { index: false }));
 
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 app.use(function (req, res, next) {
-  res.status(404).json({message: "We couldn't find what you were looking for 😞"})
-})
+	res.status(404).render('error');
+});
 
 app.use(function (err, req, res, next) {
-  console.error(err.stack)
-  res.status(500).json(err)
-})
+	console.error(err.stack);
+	res.status(500).render('error');
+});
 
 export default app;
